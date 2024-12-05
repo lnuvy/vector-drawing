37405747b5e8366dcb58255090574c856366148c
lnuvy <lnuvy.code@gmail.com>
2024-12-05 18:41:31 +0900

    tailwindcss 적용

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <>
-      <div>
+      <div className='bg-slate-700'>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
